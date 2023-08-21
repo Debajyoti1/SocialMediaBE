3861aa60b1910a3f07e85de17cade1e0a461be54
Debajyoti Dutta <debajyotidutta.50@gmail.com>
2023-08-21 22:20:24 +0530

    sign up and login

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,20 +1,22 @@
-const User = require('../../../models/user')
+const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-module.exports.login = async (req, res) => {
+
+//Create JWT token for a user
+module.exports.signin = async (req, res) => {
 
     try {
         let existingUser = await User.findOne({ email: req.body.email })
-        if (!existingUser || existingUser.password!=req.body.password) {
+        if (!existingUser || existingUser.password != req.body.password) {
             return res.status(401).json({
                 message: 'Invalid credential'
             })
         }
-        else{
+        else {
             return res.status(200).json({
                 message: 'Sign in successful',
                 data: {
-                    token: jwt.sign(existingUser.toJSON(),process.env.SECRET || 'sample-secret123',{expiresIn: '1d'})
+                    token: jwt.sign(existingUser.toJSON(), process.env.SECRET || 'sample-secret123', { expiresIn: '1d' })
                 }
             })
         }
@@ -26,3 +28,26 @@ module.exports.login = async (req, res) => {
     }
 }
 
+//Create a user and store in DB
+module.exports.signup = async (req, res) => {
+    try {
+        const existingUser = await User.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(500).json({
+                message: 'Email already exists'
+            })
+        }
+        await User.create(req.body);
+        return res.status(200).json({
+            message: 'Sign up successful',
+            user: {
+                email: req.body.email
+            }
+        })
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({
+            message: 'Something went wrong'
+        })
+    }
+}
